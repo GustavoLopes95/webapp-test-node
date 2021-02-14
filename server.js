1f5710832bd6c88d4e4bc63bf9e529fd7e888b34
unknown <luizgustavodeamorimlopes@gmail.com>
2021-02-14 17:01:12 -0300

    base commite

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+const path = require('path');
+const express = require('express');
+
+// Constants
+const PORT = 8081;
+const HOST = '127.0.0.1';
+
+const server = express();
+server.use(express.static(path.join(__dirname, './public/')));
+
+server.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/', 'index.html'));
+});
+
+server.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
